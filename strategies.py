e4cd46154c7eeeda4668118252754c365081ed3a
FelipeMarelli <fmarelli@fi.uba.ar>
2021-09-30 14:23:13 -0300

    Update strategies.py

--- strategies.py
+++ strategies.py
@@ -7,7 +7,9 @@ import chess
 from chess.engine import PlayResult
 import random
 from engine_wrapper import EngineWrapper
+import math
 
+Infinity = 100000
 
 class FillerEngine:
     """
@@ -68,7 +70,7 @@ class MinimalEngine(EngineWrapper):
         }
 
         #ERROR SI EMPIEZA DSDE OTRA POS
-        self.arbol = Arbol(chess.Board(chess.STARTING_FEN), 4)
+        #self.arbol = Arbol(chess.Board(chess.STARTING_FEN), 2)
 
     def search_with_ponder(self, board, wtime, btime, winc, binc, ponder, draw_offered):
         timeleft = 0
@@ -106,15 +108,18 @@ class Minimax(MinimalEngine):
     def search(self, board, timeleft, *otros):
         
         #falla si arracna el bot
-        self.arbol.actualizar_raiz(board.peek())
+        #self.arbol.actualizar_raiz(board.peek())
 
-        self.arbol.buscar(2)
-        #print("nodos visitados: " + str(arbol.nodos_totales))
-        #print("evaluacion: " + str(arbol.raiz.evaluacion))
+        self.arbol = Arbol(board, 3)
+
+        self.arbol.buscar(3)
+        print("nodos totales: " + str(self.arbol.nodos_totales))
+        print("nodos evaluados: " + str(self.arbol.nodos_evaluados))
         print("Profundidad: " + str(self.arbol.profundidad))
+        print("evaluacion: " + str(self.arbol.raiz.evaluacion))
 
         mejor_mov = self.arbol.raiz.mejor_movida
-        self.arbol.actualizar_raiz(mejor_mov)
+        #self.arbol.actualizar_raiz(mejor_mov)
         return PlayResult(mejor_mov, None)
         #return PlayResult(random.choice(list(board.legal_moves)), None)
 
@@ -123,9 +128,15 @@ class Arbol():
     def __init__(self, posicion_inicial, prof_inicial):
         
         print("genrando arbol")
+        self.nodos_totales = 1
+        self.nodos_evaluados = 0
         self.profundidad = 0
-        self.raiz = Nodo(posicion_inicial)
+        self.raiz = Nodo(posicion_inicial, None)
+        """if posicion_inicial.ply() > 0:
+            self.raiz = Nodo(posicion_inicial, posicion_inicial.peek())
+        else:"""
         self.expandir_niveles(prof_inicial)
+        
 
     #GENERAR ARBOL EN BASE A NODOS VISITADOS
     def expandir_niveles(self, niveles_a_agregar):
@@ -136,25 +147,26 @@ class Arbol():
     def expandir_un_nivel(self, nodo_actual):
         if len(nodo_actual.hijos()) == 0:
             nodo_actual.generar_hijos()
+            self.nodos_totales += len(nodo_actual.hijos())
             return
         for hijo in nodo_actual.hijos():
             self.expandir_un_nivel(hijo)
 
     def actualizar_raiz(self, ultima_movida):
         for hijo in self.raiz.hijos():
-            #EL NODO YA LO TENDRIA QUE SABER, ademas rompe encapsulamiento
-            if hijo.board.peek() == ultima_movida:
+            if hijo.ult_movida == ultima_movida:
                 self.raiz = hijo
                 self.profundidad -= 1
-                print("Nueva raiz: " + hijo.board.peek().uci())
+                print("Nueva raiz: " + hijo.ult_movida.uci())
                 return
 
     def buscar(self, profundidad):
         print("buscando")
 
-        self.expandir_niveles(profundidad)
+        if self.profundidad < profundidad:
+            self.expandir_niveles(profundidad - self.profundidad)
 
-        self.minimax(self.raiz)
+        self.minimax(self.raiz, profundidad, -Infinity, Infinity)
 
     """def expandir_recursivo(self, nodo_actual, prof_actual):
         if prof_actual > self.prof_max:
@@ -165,35 +177,38 @@ class Arbol():
             self.expandir_recursivo(i, prof_actual + 1)
         self.nodos_totales += len(nodo_actual.hijos())"""
 
-    def minimax(self, nodo_actual): #hacerlo con profundidad
+    def minimax(self, nodo_actual, prof, alpha, beta): #hacerlo con profundidad
 
-        if (len(nodo_actual.hijos()) == 0) or (nodo_actual.terminada):
+        if prof == 0 or (len(nodo_actual.hijos()) == 0) or (nodo_actual.terminada):
+            self.nodos_evaluados += 1
             return nodo_actual.evaluar()
 
+        #MAXIMIZER
         if nodo_actual.turno_blancas:
-            mejor_eval = -100000
-            for hijo in nodo_actual.hijos():
-                evaluacion = self.minimax(hijo)
-                #print("hoja eval " + str(evaluacion))
+            mejor_eval = -Infinity
+            for hijo in nodo_actual.hijos(): 
+                evaluacion = self.minimax(hijo, prof -1, alpha, beta)
                 if evaluacion > mejor_eval:
                     mejor_eval = evaluacion
-                    mejor_movida = hijo.board.peek()
-                if evaluacion == 1000: 
+                    mejor_movida = hijo.ult_movida
+                if mejor_eval > alpha:
+                    alpha = mejor_eval
+                if beta <= alpha: 
                     break
-        else:
-            mejor_eval = 100000
+        else: #MINIMIZER
+            mejor_eval = Infinity
             for hijo in nodo_actual.hijos():
-                evaluacion = self.minimax(hijo)
+                evaluacion = self.minimax(hijo, prof -1, alpha, beta)
                 if evaluacion < mejor_eval:
                     mejor_eval = evaluacion
-                    mejor_movida = hijo.board.peek()
-                if evaluacion == -1000: 
-                    break
+                    mejor_movida = hijo.ult_movida
+                if beta <= alpha:
+                    break       
         nodo_actual.evaluacion = mejor_eval
         nodo_actual.mejor_movida = mejor_movida
         return mejor_eval
 
-        #la versiona anterior
+    #la versiona anterior
         """
         for hijo in nodo_actual.hijos():
             self.minimax(hijo)
@@ -222,14 +237,16 @@ class Arbol():
 
 
 class Nodo():
-    def __init__(self, board):
+    def __init__(self, board, movida):
         self.board = board
+        self.ult_movida = movida
         self.nodos_hijos = []
         self.turno_blancas = (board.turn == chess.WHITE)
         outcome = self.board.outcome()
         self.terminada = not (outcome == None)
         if(self.terminada):
             self.gano_blancas = bool(outcome.winner == chess.WHITE)
+            self.gano_negras = bool(outcome.winner == chess.BLACK)
         self.evaluacion = False
         self.mejor_movida = False
 
@@ -238,38 +255,54 @@ class Nodo():
 
     def generar_hijos(self):
         movidas = list(self.board.legal_moves)
-        for i in movidas:
+        for mov in movidas:
             nuevo_board = self.board.copy()
-            nuevo_board.push(i)
-            nuevo_nodo = Nodo(nuevo_board)
-            self.nodos_hijos.append(nuevo_nodo)
+            nuevo_board.push(mov)
+            nuevo_nodo = Nodo(nuevo_board, mov)
+            if nuevo_board.is_check():
+                self.nodos_hijos.insert(0, nuevo_nodo)
+            else:
+                self.nodos_hijos.append(nuevo_nodo)
 
     def evaluar(self):
-        eval_negras = 0
-        eval_blancas = 0
-        material = 0
+        evaluacion = 0
         #jaque mate
         if self.terminada:
             if self.gano_blancas:
-                eval_blancas = 1000
+                evaluacion = 1000
+            elif self.gano_negras:
+                evaluacion = -1000
             else:
-                eval_negras = 1000
+                evaluacion = 0
         else: 
             #material
-            #print(self.board.board_fen())
-            material = self.contar_material(self.board.board_fen())
+            evaluacion += self.contar_material(self.board.board_fen())
 
+            #movidas
+            if self.turno_blancas:
+                movidas_blancas = self.board.legal_moves.count()
+                tablero_turno_cambiado = self.board.copy()
+                tablero_turno_cambiado.push(chess.Move.null())
+                movidas_negras = tablero_turno_cambiado.legal_moves.count()
+            else:
+                movidas_negras = self.board.legal_moves.count()
+                tablero_turno_cambiado = self.board.copy()
+                tablero_turno_cambiado.push(chess.Move.null())
+                movidas_blancas = tablero_turno_cambiado.legal_moves.count()
+            #evaluacion += math.tanh((movidas_blancas - movidas_negras) * 0.2)*0.5
+            evaluacion += (((movidas_blancas - movidas_negras)+20)/(40))*0.5
+                
             #enroque
             """if self.board.has_castling_rights(chess.WHITE):
                 eval_blancas += 0.5
             if self.board.has_castling_rights(chess.BLACK):
-                eval_negras += 0.5"""
-        self.evaluacion = material + eval_blancas - eval_negras
+                eval_negras += 0.5"""   
+        self.evaluacion = evaluacion
         return self.evaluacion
 
     def contar_material(self, fen):
         material = 0
-        valores_piezas = {"p": -1, "n": -3, "b": -3, "r": -5, "q": -9, "P": 1, "N": 3, "B": 3, "R": 5, "Q": 9}
+        valores_piezas = {"p": -1, "n": -3, "b": -3.1, "r": -5, "q": -9, "P": 1, "N": 3, "B": 3.1, "R": 5, "Q": 9}
         for i in fen:
             if i in valores_piezas:
                 material += valores_piezas[i]
